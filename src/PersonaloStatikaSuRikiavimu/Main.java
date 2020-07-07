aaef59dfb25ab1717f687ae5b99b7acdc9a9386f
Mindaugas <darbas.mindaugas@gmail.com>
2020-07-07 12:21:32 +0300

    Adding task

---
+++ src/PersonaloStatikaSuRikiavimu/Main.java
@@ -0,0 +1,7 @@
+package PersonaloStatikaSuRikiavimu;
+
+// Personalo statisikos uždaviniu remdamiesi susikurkime ArrayList<Darbuotojas> (nuskaitome iš failo)
+// Išrikiuokime tą listą pagal darbuotojo atlygį
+// Ir išrikiuotus duomenis įrašykime į failą
+public class Main {
+}
